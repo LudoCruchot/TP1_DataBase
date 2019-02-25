add84dcb5de7ef531d54ff944f8da5850553561e
LudoCruchot <32196302+LudoCruchot@users.noreply.github.com>
2019-02-25 00:08:38 -0500

    various fixes

--- scrapping/index.js
+++ scrapping/index.js
@@ -1,6 +1,5 @@
-const mongoUtil = require('../utils/mongo');
 const launchScrapping = require('./extractor');
 
 console.log(">>LAUNCHING SCRAPPING SCRIPT");
 
-mongoUtil.connect().then(launchScrapping);
+launchScrapping();
